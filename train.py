0edbcdab0e1756c1912a153207691c6170ed8086
micah <micahpriddis@gmail.com>
2020-04-21 10:57:18 -0700

    adding file to repo

---
+++ train.py
@@ -0,0 +1,139 @@
+
+import numpy as np 
+import tensorflow as tf
+import tf_agents.networks.q_network
+from tf_agents.agents.dqn import dqn_agent
+from tf_agents.drivers import dynamic_step_driver
+from tf_agents.environments import suite_gym
+from tf_agents.environments import tf_py_environment
+from tf_agents.eval import metric_utils
+from tf_agents.metrics import tf_metrics
+from tf_agents.networks import q_network
+from tf_agents.policies import random_tf_policy
+from tf_agents.replay_buffers import tf_uniform_replay_buffer
+from tf_agents.trajectories import trajectory
+from tf_agents.utils import common
+from tf_agents.specs import tensor_spec
+
+def compute_avg_return(environment, policy, num_episodes=10):
+
+  total_return = 0.0
+  for _ in range(num_episodes):
+
+    time_step = environment.reset()
+    episode_return = 0.0
+
+    while not time_step.is_last():
+      action_step = policy.action(time_step)
+      time_step = environment.step(action_step.action)
+      episode_return += time_step.reward
+    total_return += episode_return
+
+  avg_return = total_return / num_episodes
+  return avg_return.numpy()[0]
+
+'''
+    SET ENVIROMENTS
+
+'''
+
+train_env = suite_gym.load("SpaceInvaders-ram-v0")
+eval_env = suite_gym.load("SpaceInvaders-ram-v0")
+train_env.seed(0)
+#train_env = tf_py_environment.TFPyEnvironment(train_py_env)
+print(train_env.observation_spec())
+# print(train_env.observation_spec())
+# print(train_env.action_spec())
+# print(tensor_spec.from_spec(train_env.action_spec()))
+# exit()
+
+
+'''
+    HYPERPARAMTERS
+
+'''
+
+initial_collect_steps = 1000  # @param {type:"integer"} 
+collect_steps_per_iteration = 1  # @param {type:"integer"}
+replay_buffer_max_length = 100000  # @param {type:"integer"}
+
+batch_size = 64  # @param {type:"integer"}
+learning_rate = 1e-3  # @param {type:"number"}
+log_interval = 200  # @param {type:"integer"}
+
+num_eval_episodes = 10  # @param {type:"integer"}
+eval_interval = 1000  # @param {type:"integer"}
+
+fc_layer_params = (100,)
+
+'''
+    INITIALIZE DQNETWORK
+
+'''
+
+q_net = q_network.QNetwork(
+    tensor_spec.from_spec(train_env.observation_spec()),
+    tensor_spec.from_spec(train_env.action_spec()),
+    fc_layer_params=fc_layer_params)
+
+
+optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+
+train_step_counter = tf.Variable(0)
+
+agent = dqn_agent.DqnAgent(
+    train_env.time_step_spec(),
+    tensor_spec.from_spec(train_env.action_spec()),
+    q_network=q_net,
+    optimizer=optimizer,
+    td_errors_loss_fn=common.element_wise_squared_loss,
+    train_step_counter=train_step_counter)
+
+agent.initialize()
+
+'''
+    SET POLICIES
+
+'''
+
+
+eval_policy = agent.policy
+collect_policy = agent.collect_policy
+
+
+'''
+    TRAIN
+
+'''
+
+train_env.reset()
+
+# (Optional) Optimize by wrapping some of the code in a graph using TF function.
+agent.train = common.function(agent.train)
+
+# Reset the train step
+agent.train_step_counter.assign(0)
+
+# Evaluate the agent's policy once before training.
+avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+returns = [avg_return]
+
+for _ in range(num_iterations):
+
+  # Collect a few steps using collect_policy and save to the replay buffer.
+  for _ in range(collect_steps_per_iteration):
+    collect_step(train_env, agent.collect_policy, replay_buffer)
+
+  # Sample a batch of data from the buffer and update the agent's network.
+  experience, unused_info = next(iterator)
+  train_loss = agent.train(experience).loss
+
+  step = agent.train_step_counter.numpy()
+
+  if step % log_interval == 0:
+    print('step = {0}: loss = {1}'.format(step, train_loss))
+
+  if step % eval_interval == 0:
+    avg_return = compute_avg_return(eval_env, agent.policy, num_eval_episodes)
+    print('step = {0}: Average Return = {1}'.format(step, avg_return))
+    returns.append(avg_return)
